495a9ba593a40434fc7339092f2f683e16349420
tbelton1160 <travithebest@gmail.com>
2023-12-15 23:49:41 -0500

    8kyu update

---
+++ 8kyu/hex.to.decimal.js
@@ -0,0 +1,7 @@
+//Complete the function which converts hex number (given as a string) to a decimal number.
+
+//Solution
+function hexToDec(hexString){
+    //your code here
+    return parseInt(hexString, 16)
+  }
